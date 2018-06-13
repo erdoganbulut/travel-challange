349e07c813dfdbd1b634fe24e673a08dac17e18a
Erdogan Bulut <e-bulut@hotmail.com.tr>
2018-06-13 13:44:34 +0300

    Integrate posts and comments service

--- src/app/routes/start/start.component.ts
+++ src/app/routes/start/start.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { PostsService } from '../../service/posts.service';
+import { CommentsService } from '../../service/comments.service';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  posts: any[];
+  postsSubscription: Subscription;
+
+  comments: any[];
+  commentsSubscription: Subscription;
+
+  constructor(private http: HttpClient,
+    private _postsService: PostsService,
+    private _commentsService: CommentsService) { }
 
   ngOnInit() {
+    this._postsService.getPosts();
+    this.postsSubscription = this._postsService.posts$.subscribe((value) => {
+      this.posts = value
+    });
+
+    this._commentsService.getComments();
+    this.commentsSubscription = this._commentsService.comments$.subscribe((value) => {
+      this.comments = value
+    })
   }
 
 }
