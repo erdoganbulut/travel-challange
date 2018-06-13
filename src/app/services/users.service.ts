f7059b64727c7517e1fecd83c9d43f3975de05df
Erdogan Bulut <e-bulut@hotmail.com.tr>
2018-06-13 14:39:33 +0300

    Fix users  service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private _usersSource = new BehaviorSubject<any>([]);
+  users$ = this._usersSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe(
+      (value: any) => {
+        this._usersSource.next(value);
+      },
+      (error: any) => {
+        this._usersSource.next([]);
+      }
+    );
+  }
+
 }
