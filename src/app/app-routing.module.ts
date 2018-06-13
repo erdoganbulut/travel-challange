2673ac483f550ff3237961074b5d6aef661c6342
Erdogan Bulut <e-bulut@hotmail.com.tr>
2018-06-13 14:29:06 +0300

    Route posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from './routes/start/start.component';
+import { PostsComponent } from './routes/posts/posts.component';
 
 const routes: Routes = [
   {
     path: '',
     component: StartComponent,
+  },
+  {
+    path: 'posts',
+    component: PostsComponent,
   },
 ];
 
