13865827ee07bed23237b5c2d4d53cf35a202b15
Erdogan Bulut <e-bulut@hotmail.com.tr>
2018-06-13 14:28:42 +0300

    Fix posts component

--- src/app/routes/posts/posts.component.ts
+++ src/app/routes/posts/posts.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PostsService } from '../../service/posts.service';
+import { CommentsService } from '../../service/comments.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: any[];
+  postsSubscription: Subscription;
+
+  comments: any[];
+  commentsSubscription: Subscription;
+
+  constructor(private _postsService: PostsService,
+    private _commentsService: CommentsService) { }
+  
+  fillPosts() {
+    let val = _.cloneDeep(this.posts);
+    val.forEach(element => {
+      element.comments = _.filter(this.comments, { 'postId': element.id }).length
+    });
+    this.posts = val;
+  }
 
   ngOnInit() {
+    this._postsService.getPosts();
+    this.postsSubscription = this._postsService.posts$.subscribe((value) => {
+      this.posts = value;
+      this.fillPosts();
+    });
+
+    this._commentsService.getComments();
+    this.commentsSubscription = this._commentsService.comments$.subscribe((value) => {
+      this.comments = value;
+      this.fillPosts();
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSubscription.unsubscribe();
+    this.commentsSubscription.unsubscribe();
   }
 
 }
