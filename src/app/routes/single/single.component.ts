2ef894cf062c679cdb9a3e973f150d9a6ff84cd4
Erdogan Bulut <e-bulut@hotmail.com.tr>
2018-06-18 04:45:47 +0300

    fix single ts

--- src/app/routes/single/single.component.ts
+++ src/app/routes/single/single.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import * as _ from 'lodash';
+import { SingleService } from '../../services/single.service';
+import { CommentsService } from '../../services/comments.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-single',
@@ -11,15 +16,56 @@ export class SingleComponent implements OnInit {
   id: number;
   private routeSub: any;
 
-  constructor(private route: ActivatedRoute) { }
+  post: { id, comments, user, userId };
+  postSubscription: Subscription;
+
+  comments: any[];
+  commentsSubscription: Subscription;
+
+  users: any[];
+  usersSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute,
+    private _singleService: SingleService,
+    private _commentsService: CommentsService,
+    private _usersService: UsersService) { }
 
   dedectId(id: number) {
-    console.log(id)
+    this.id = id;
+    this._singleService.getPost(id);
+    this.fillPost();
+  }
+
+  fillPost() {
+    if (this.post && this.users && this.comments) {
+      let user = _.find(this.users, { 'id': this.post.userId });
+      if (user) {
+        this.post.user = `${user.username} (${user.name})`;
+        this.post.comments = _.filter(this.comments, { 'postId': this.post.id });
+      }
+    }
   }
 
   ngOnInit() {
     this.routeSub = this.route.params.subscribe((params) => {
       this.dedectId(+params['id']); // '+' for string convert to number
+    });
+
+    this.postSubscription = this._singleService.post$.subscribe((post) => {
+      this.post = post;
+      this.fillPost();
+    });
+
+    this._commentsService.getComments();
+    this.commentsSubscription = this._commentsService.comments$.subscribe((value) => {
+      this.comments = value;
+      this.fillPost();
+    });
+
+    this._usersService.getUsers();
+    this.usersSubscription = this._usersService.users$.subscribe((value) => {
+      this.users = value;
+      this.fillPost();
     });
   }
 
